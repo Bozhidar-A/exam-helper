52138696ec2236cc430717658673e38dd259eca2
BozhidarA <bplatanasov@gmail.com>
2022-03-20 02:16:00 +0200

    updated db, added funcs for creating text and calculating score for checking
    TODO:
    Update onChange to give proper points

--- src/components/BEL_FindMissingPunctuation.tsx
+++ src/components/BEL_FindMissingPunctuation.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import styles from "../css/main.module.css"
+import PointsFromQuestion from "./PointsFromQuestion";
 
 function BEL_FindMissingPunctuation(props:any){
 
@@ -25,6 +26,28 @@ function BEL_FindMissingPunctuation(props:any){
         }
     }
 
+    function FormatCorrectText() {
+        var tmp:string = props.data.wrong;
+
+        props.data.punctuation.map((pu:any) => {
+            tmp = tmp.substring(0, pu.index) + `<b>${pu.symbol}</b>` + tmp.substring(pu.index)
+        })
+
+        return tmp
+    }
+
+    function CheckingDisplayScore(){
+        var score:number = 0;
+
+        props.data.punctuation.map((pu:any) => {
+            if(inputText[pu.index] === pu.symbol){
+                score++;
+            }
+        })
+
+        return score;
+    }
+
     return(<div className={styles.EveryExamComponent}>
         <p>{props.data.question}</p>
         <br />
@@ -33,11 +56,13 @@ function BEL_FindMissingPunctuation(props:any){
         <textarea className={styles.TextareaMaxWidth} disabled={props.checking} onChange={e => {HandleChange(e.target.value)}}></textarea>
         {props.checking && 
             <div>
-                <textarea className={`bg-lime-500 ${styles.TextareaMaxWidth}`} disabled={true} value={props.data.correct}></textarea>
-                {props.checking && <p>От този въпрос вие взехте {inputText === props.data.correct ? 5 : 0} точки</p>}
+                <pre className={`bg-lime-500`} dangerouslySetInnerHTML={{ __html: FormatCorrectText() }}></pre>
+                <PointsFromQuestion points={CheckingDisplayScore()}></PointsFromQuestion>
+                {/* <textarea className={`bg-lime-500 ${styles.TextareaMaxWidth}`} disabled={true} value={props.data.correct}></textarea>
+                {props.checking && <p>От този въпрос вие взехте {inputText === props.data.correct ? 5 : 0} точки</p>} */}
                 {/* TODO write better points get for this */}
             </div>}
     </div>)
 }
 
-export default BEL_FindMissingPunctuation;
+export default React.memo(BEL_FindMissingPunctuation);
